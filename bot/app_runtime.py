e6cac0389bccb2e0e67695055af8757911e3fbc7
UC - Aayan Boradia <aayan.boradia@unidesignusa.com>
2026-08-20 10:30:55 -0700

    Remove win32cred and use env Fenix credentials

--- bot/app_runtime.py
+++ bot/app_runtime.py
@@ -27,7 +27,7 @@ class KeyzarFenixApp(tk.Tk):
     def __init__(self) -> None:
         super().__init__()
 
-        self.title("Keyzar Fenix Bot - v1")
+        self.title("Keyzar Fenix Bot - v1Testing")
         self.geometry("960x700")
         self.minsize(860, 600)
 
